2f175eb71976df5a8c8b1af7a4c3d2642b0a9e6e
root <fonso.gonzalezsan@gmail.com>
2020-05-24 05:58:43 -0400

    extract targeted links without replies and stored on local program list, modify # references, make full path from relative path and remove external reference links

--- spider.py
+++ spider.py
@@ -2,6 +2,7 @@
 
 import requests
 import re
+import urlparse
 
 def request(url, protocol):
     try:
@@ -9,11 +10,21 @@ def request(url, protocol):
     except Exception:
         pass
 
-target_url ="marca.com"
+def extract_web_links(target_url, protocol):
+    response = request(target_url, protocol)
+    print("[+] web target ==> " + protocol + target_url)
+    return re.findall('(?:href=")(.*?)"', response.content)
+
+
+target_url ="yourweb..."
 protocol ="https://"
-response = request(target_url, protocol)
-print("[+] web target ==> "+protocol+target_url)
-href_links =re.findall('(?:href=")(.*?)"', response.content)
+list_dicover_links = []
+href_links = extract_web_links(target_url, protocol)
 
 for link in href_links:
-    print("\t [+] reference link ==> "+link)
+    link = urlparse.urljoin(protocol+target_url,link)
+    if "#" in link:
+        link = link.split("#")[0]+link.split("#")[1]
+    if target_url in link and link not in list_dicover_links:
+        list_dicover_links.append(link)
+        print("\t [+] reference link ==> "+link)
