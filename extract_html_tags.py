7dfa97278fd177feaae49b0ede4fbddb4fb978ca
root <fonso.gonzalezsan@gmail.com>
2020-05-24 08:54:16 -0400

    execution extract html finishd with post: cat extract_html_tags_execute_post.txt http://10.0.9.6/mutillidae/index.php?page=dns-lookup.php 	[+] post url ==> http://10.0.9.6/mutillidae/index.php?page=dns-lookup.php 	[+] action ==> index.php?page=dns-lookup.php 	[+] method type ==> post 		 [+] form input input_name ==> target_host 		 [+] form input  input_type ==> text 		 [+] form input input_value ==> None 		 [+] form input input_name ==> dns-lookup-php-submit-button 		 [+] form input  input_type ==> submit 		 [+] form input input_value ==> Lookup DNS <Response [200]>

--- extract_html_tags.py
+++ extract_html_tags.py
@@ -2,7 +2,7 @@
 
 import requests
 from bs4 import BeautifulSoup
-
+import urlparse
 
 def request(url):
     try:
@@ -18,15 +18,25 @@ try:
     if response.content:
         parsed_html = BeautifulSoup(response.content)
         forms_list = parsed_html.findAll("form")
+        post_data={}
         for form in forms_list:
             action = form.get("action")
+            post_url = urlparse.urljoin(target_url,action)
+            print("\t[+] post url ==> "+post_url)
             print("\t[+] action ==> "+action)
             method = form.get("method")
             print("\t[+] method type ==> "+method)
             input_list= form.findAll("input")
             for input in input_list:
-                print("\t\t [+] form inputs ==> "+input)
-
+                input_name = input.get("name")
+                input_type = input.get("type")
+                input_value= input.get("value")
+                print("\t\t [+] form input input_name ==> " + str(input_name))
+                print("\t\t [+] form input  input_type ==> " + str(input_type))
+                print("\t\t [+] form input input_value ==> " + str(input_value))
+                post_data[input_name] = input_value
+            result= requests.post(post_url,data=post_data)
+            print(result)
 except Exception as e:
     print(e)
     pass
